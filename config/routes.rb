08757be96fd7cbd64108070c7fe8780b45eb1d34
arslanshahid114 <arslan.shahid@amroodlabs.com>
2023-01-03 18:11:16 +0500

    Added Devise(Controllers,views,models)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users ,controllers: {
+        sessions: 'users/sessions'
+      }
+
+  # devise_scope :users do
+  #   root "users/sessions#new"
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
